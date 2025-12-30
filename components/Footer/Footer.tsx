96f5f9dfc2a5fa142f00d3ff73d97954f205938f
Jai Irkal <67275191+JaiIrkal@users.noreply.github.com>
2025-12-30 07:01:29 +0530

    Responsive Footer UI Update

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,13 +4,18 @@ import { footer_content } from "@/types/static-data";
 export default function Footer() {
   return (
     <div className="flex flex-col bg-black w-full">
-      
+
       {/* Top Footer */}
-      <div className="flex flex-col lg:flex-row w-full justify-between gap-12 lg:gap-16 
-                      px-6 sm:px-12 lg:px-56 py-12">
-        
+      <div
+        className="w-full px-6 sm:px-12 lg:px-56 py-12
+                   flex flex-col lg:flex-row justify-between gap-12 lg:gap-16"
+      >
         {/* Footer Links */}
-        <div className="flex flex-col sm:flex-row gap-8">
+        <div
+          className="grid grid-cols-2 gap-8
+                     sm:grid-cols-2
+                     lg:flex lg:gap-8"
+        >
           {footer_content.map((group) => (
             <div key={group.id}>
               <h4 className="text-[14px] text-white">{group.title}</h4>
@@ -30,7 +35,7 @@ export default function Footer() {
         </div>
 
         {/* Social */}
-        <div>
+        <div className="hidden sm:block">
           <h4 className="text-[14px] text-white">FOLLOW US</h4>
           <div className="flex mt-2 gap-3">
             <Link href="https://www.linkedin.com/company/arcido">
@@ -52,17 +57,43 @@ export default function Footer() {
       </div>
 
       {/* Bottom Footer */}
-      <div className="flex flex-col items-start px-6 sm:px-12 lg:px-16 pb-8">
-        <img
-          src="images/arcido-logo.png"
-          className="w-[82px] h-[82px]"
-          alt="Footer Logo"
-        />
+      <div className="px-6 sm:px-12 lg:px-16 pb-8">
+        {/* Logo + Social (mobile row) */}
+        <div className="flex items-center justify-between sm:justify-start sm:gap-6">
+          <img
+            src="images/arcido-logo.png"
+            className="w-[82px] h-[82px]"
+            alt="Footer Logo"
+          />
+
+          {/* Social (mobile only here) */}
+          <div className="-mt-5">
+            <h4 className="text-[14px] text-white mb-2 sm:hidden">FOLLOW US</h4>
+            <div className="flex gap-3 sm:hidden">
+            <Link href="https://www.linkedin.com/company/arcido">
+              <img
+                src="images/social-media/linkedin.svg"
+                className="w-[20px] h-[20px]"
+                alt="LinkedIn"
+              />
+            </Link>
+            <Link href="https://www.instagram.com/the.arcido/">
+              <img
+                src="images/social-media/instagram.png"
+                className="w-[20px] h-[20px]"
+                alt="Instagram"
+              />
+            </Link>
+          </div>
+          </div>
+        </div>
 
         <div className="border-t border-[#262626] w-full mt-4"></div>
 
-        <div className="flex flex-col sm:flex-row justify-between w-full mt-4 
-                        text-xs text-gray-500 gap-2 sm:gap-0">
+        <div
+          className="flex flex-col sm:flex-row justify-between w-full mt-4
+               text-xs text-gray-500 gap-2 sm:gap-0"
+        >
           <p>Copyright © 2025 Arcido, Inc. All rights reserved.</p>
           <p>Terms of Use & Privacy Policy</p>
         </div>
