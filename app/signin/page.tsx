1336230ef60f32dbf840fac79f4aec25f7a66287
Jai Irkal <67275191+JaiIrkal@users.noreply.github.com>
2026-01-01 03:36:22 +0530

    Test

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -11,19 +11,7 @@ const Lottie = dynamic(() => import("lottie-react"), {
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-transparent text-white text-center px-4">
-      <Lottie
-        animationData={launchingAnimation}
-        loop
-        className="w-72 h-72 mb-8"
-      />
-
-      <h1 className="text-5xl md:text-6xl font-bold tracking-tight mb-2">
-        Launching Soon
-      </h1>
-
-      <p className="text-lg opacity-80">
-        Sign in
-      </p>
+      <h1 className="relative text-4xl bottom-20">404 Page Not Found</h1>
     </main>
   );
 }
