246a4b7c07d16e911ba3c02b4c6f6c9705f93482
Jai Irkal <67275191+JaiIrkal@users.noreply.github.com>
2026-01-01 03:58:21 +0530

    fixed

--- types/static-data.ts
+++ types/static-data.ts
@@ -45,11 +45,11 @@ export const footer_content = [
         links:[
             {
                 label:"About Us",
-                url:"company/about-us"
+                url:"/company/about-us"
             },
             {
                 label:"Careers",
-                url:"company/careers"
+                url:"/company/careers"
             }
         ]
     },
@@ -59,7 +59,7 @@ export const footer_content = [
         links:[
             {
                 label:"Blog",
-                url:"resources/blog"
+                url:"/resources/blog"
             },
             // {
             //     label:"Documentation",
@@ -67,7 +67,7 @@ export const footer_content = [
             // },
             {
                 label:"Events",
-                url:"resources/events"
+                url:"/resources/events"
             }
         ]
     }
